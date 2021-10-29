2d40eb7d62d3c115fc1825c8c9d90e36351bef09
Enma <a20151887@pucp.edu.pe>
2021-10-28 20:55:35 -0500

    borrando cosas

--- src/routers/index.js
+++ src/routers/index.js
@@ -8,14 +8,8 @@ import ErrorNotFound from '../components/layout/ErrorNotFound/index.vue';
 import Login from '../components/layout/Login/index.vue';
 
 import Pedidos from '../components/layout/EnrutaGLP/Pedidos/index.vue';
-import Mediciones from '../components/layout/EnrutaGLP/Mediciones/index.vue';
-import MedicionesDetalle from '../components/layout/EnrutaGLP/Mediciones/MedicionesDetalle/index.vue';
-import Evidencias from '../components/layout/EnrutaGLP/Mediciones/MedicionesDetalle/Evidencias/index.vue';
-import PlanMedicion from '../components/layout/EnrutaGLP/PlanMedicion/index.vue';
-import VerMedicion from '../components/layout/EnrutaGLP/PlanMedicion/VerMedicion/index.vue';
 import OperacionDiaADia from '../components/layout/EnrutaGLP/OperacionDiaADia/index.vue';
 import SimulacionTresDias from '../components/layout/EnrutaGLP/SimulacionTresDias/index.vue';
-import ReestablecerContrasena from '../components/layout/Login/reestablecerContrasena/index.vue';
 
 Vue.use(Router);
 
@@ -26,11 +20,6 @@ export default new Router({
             name: 'Login',
             component: Login,
             
-        },
-        {
-            path: '/reestablecerContrasena',
-            name: 'ReestablecerContrasena',
-            component: ReestablecerContrasena,
         },
         {
             path: '/',
@@ -57,31 +46,6 @@ export default new Router({
                     path: 'enruta-glp/OperacionDiaADia/',
                     name: 'OperacionDiaADia',
                     component: OperacionDiaADia,
-                },
-                {
-                    path: 'enruta-glp/PlanMedicion/',
-                    name: 'PlanMedicion',
-                    component: PlanMedicion,
-                },
-                {
-                    path:'enruta-glp/PlanMedicion/VerMedicion/:id',
-                    name:'VerMedicion',
-                    component: VerMedicion,
-                },
-                {
-                    path: 'enruta-glp/Mediciones',
-                    name: 'Mediciones',
-                    component: Mediciones,
-                },
-                {
-                    path: 'enruta-glp/Mediciones/MedicionesDetalle/:idMuestra',
-                    name: 'MedicionesDetalle',
-                    component: MedicionesDetalle,
-                },
-                {
-                    path: 'enruta-glp/MedicionesDetalle/Evidencias',
-                    name: 'Evidencias',
-                    component: Evidencias,
                 },
                 {
                     path: 'enruta-glp/SimulacionTresDias',
